77cde51c73b110a8049f3755e8bec2828b8a6e04
carloslme <32799711+carloslme@users.noreply.github.com>
2021-02-19 23:12:43 -0600

    Step 2: Fin all the results

--- chapter_11/lucky.py
+++ chapter_11/lucky.py
@@ -12,9 +12,9 @@ import requests, sys, webbrowser, bs4
 print('Googling...') # display text while downloading the Google page
 res = requests.get('https://www.google.com/search?q=' + ' '.join(sys.argv[1:]))
 res.raise_for_status()
+# Retrieve top search result links.
+soup = bs4. BeautifulSoup(res.text, 'lxml')
+# Open a browser tab for each result.
+linkElems = soup.select('.r a') # It looks like the r class is used only for search result links.
 
-print(res)
-
-# TODO: Retrieve top search result links.
-
-# TODO: Open a browser tab for each result.
+print(linkElems)
