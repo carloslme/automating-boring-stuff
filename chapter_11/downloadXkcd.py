7808b864017a583e5fee948d1c35a585476d9bb6
carloslme <32799711+carloslme@users.noreply.github.com>
2021-02-20 23:56:05 -0600

    Find and Download the Comic Image in script.

--- chapter_11/downloadXkcd.py
+++ chapter_11/downloadXkcd.py
@@ -14,8 +14,15 @@ while not url.endswith('#'):
     soup = bs4.BeautifulSoup(res.text)
     
     # TODO: Find the URL of the comic image. 
-
-    # TODO: Download the image. 
+    comicElem = soup.select('#comic img') 
+    if comicElem == []: 
+        print('Could not find comic image.') 
+    else:
+        comicUrl = 'http:' + comicElem[0].get('src')
+    # TODO: Download the image.
+    print('Downloading image %s...' % (comicUrl)) 
+    res = requests.get(comicUrl) 
+    res.raise_for_status() 
     # TODO: Save the image to ./xkcd. 
     # TODO: Get the Prev button's url. 
  
